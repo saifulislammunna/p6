75bd7e40301d5884ff21217bd9faf8194920dcaf
saifulislammunna <saifulmunna28@gmail.com>
2022-01-15 18:38:14 +0600

    first commit

---
+++ p6.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+
+{
+  int numbers[5] = {10, 20, 30, 40, 50};
+  printf("Fist element: %d\n", numbers[0]);
+  printf("Third element: %d\n", numbers[2]);
+
+
+  return 0;
+
+}
